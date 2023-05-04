06525a6222ac949f8e30dcb46f832b058b42f41d
Alina Li <alinali87@gmail.com>
2023-05-04 11:38:59 +0000

    decrease cluster memory (limit 3 TB)

--- airflow/dags/gharchive_dag.py
+++ airflow/dags/gharchive_dag.py
@@ -31,12 +31,12 @@ CLUSTER_CONFIG = {
     "master_config": {
         "num_instances": 1,
         "machine_type_uri": "e2-standard-2",
-        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024},
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 512},
     },
     "worker_config": {
         "num_instances": 2,
         "machine_type_uri": "e2-standard-2",
-        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024},
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 512},
     },
 }
 
